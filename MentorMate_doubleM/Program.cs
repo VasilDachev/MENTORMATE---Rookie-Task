1ce7f58457c9631da3c2c8b38ae0c95e872638bc
VasilDachev <vasildachevnew@gmail.com>
2019-05-11 14:38:05 +0300

    DoNe

--- MentorMate_doubleM/Program.cs
+++ MentorMate_doubleM/Program.cs
@@ -19,58 +19,58 @@ namespace MentorMate_doubleM
             
             Console.Write("Enter boldness value:");
             
-            int boldness = int.Parse(Console.ReadLine());
-            if (boldness % 2 == 0 || boldness <= 2 || boldness >= 10_000) 
+            int Thickness = int.Parse(Console.ReadLine());
+            if (Thickness % 2 == 0 || Thickness <= 2 || Thickness >= 10_000) 
             {
                 Console.WriteLine("Enter odd value between 2 and 10 000!");
                 return;
             }
 
-            int Changer=0;
+            int brUpper=0;
 
-            int stopper = (boldness / 2) + 1;
+            int stopper = (Thickness / 2) + 1;
 
-            int helper = 1;
-            int moreHelp = 1;
-            int br = boldness/2 ;
+            int brDownSide = 1;
+            int brDownSideLowCount = 1;
+            int brLowerRightEnd = Thickness/2 ;
 
             
 
-            for (int row = 0; row <= boldness; row++) 
+            for (int row = 0; row <= Thickness; row++) 
             {
                 if (row>=stopper)
                 {
-                    moreHelp++;
+                    brDownSideLowCount++;
                     
                 }
                 if (row>stopper)
                 {
-                    br++;
+                    brLowerRightEnd++;
                 }
 
                 
-                for (int column = 0; column <= 6*boldness-1; column++)
+                for (int column = 0; column <= 6*Thickness-1; column++)
                 {
                     
                     if (row<stopper)
                     {
-                        if (column < boldness - Changer)
+                        if (column < Thickness - brUpper)
                         {
                             Console.Write('-');
                         }
-                        if (column > boldness - Changer && column <= boldness * 2 + Changer)
+                        if (column > Thickness - brUpper && column <= Thickness * 2 + brUpper)
                         {
                             Console.Write('*');
                         }
-                        if (column > boldness * 2 + Changer && column <= boldness * 3 - Changer)
+                        if (column > Thickness * 2 + brUpper && column <= Thickness * 3 - brUpper)
                         {
                             Console.Write('-');
                         }
-                        if (column > boldness * 3 - Changer && column <= boldness * 4 + Changer)
+                        if (column > Thickness * 3 - brUpper && column <= Thickness * 4 + brUpper)
                         {
                             Console.Write('*');
                         }
-                        if (column > boldness * 4 + Changer && column <= boldness * 5)
+                        if (column > Thickness * 4 + brUpper && column <= Thickness * 5)
                         {
                             Console.Write('-');
                         }
@@ -79,40 +79,40 @@ namespace MentorMate_doubleM
                     if (row >= stopper)
                     {
                         
-                        if (column <= boldness - helper)
+                        if (column <= Thickness - brDownSide)
                         {
                             Console.Write('-');    
                         }
-                        if (column >boldness-helper&&column<=boldness-helper + boldness)
+                        if (column > Thickness - brDownSide && column <= Thickness - brDownSide + Thickness) 
                         {
                             Console.Write('*');
                         }
-                        
-                        if ( column >= 2*boldness - moreHelp&& column<2*boldness+moreHelp - 3 ) 
+
+                        if (column >= 2 * Thickness - brDownSideLowCount && column < 2 * Thickness + brDownSideLowCount - 3)  
                         {
                             Console.Write('-');
                         }
 
-                        if (column>=2*boldness+moreHelp-3&&column<4*boldness-moreHelp)
+                        if (column >= 2 * Thickness + brDownSideLowCount - 3 && column < 4 * Thickness - brDownSideLowCount) 
                         {
                             Console.Write('*');
                         }
-                        if (column >= 4 * boldness - moreHelp && column < 4 * boldness + moreHelp - 3 ) 
+                        if (column >= 4 * Thickness - brDownSideLowCount && column < 4 * Thickness + brDownSideLowCount - 3) 
                         {
                             Console.Write('-');
                         }
-                        if (column >= 4 * boldness + moreHelp - 3 && column <= 4 * boldness + br + (boldness / 2) - 1)
+                        if (column >= 4 * Thickness + brDownSideLowCount - 3 && column <= 4 * Thickness + brLowerRightEnd + (Thickness / 2) - 1) 
                         {
                             Console.Write('*');
                         }
-                        if (column>4*boldness+br+(boldness/2)-1&&column<boldness*5 +br-moreHelp+1)
+                        if (column > 4 * Thickness + brLowerRightEnd + (Thickness / 2) - 1 && column < Thickness * 5 + brLowerRightEnd - brDownSideLowCount + 1) 
                         {
                             Console.Write('-');
                         }
                     }
                 }
-                helper++;
-                Changer++;
+                brDownSide++;
+                brUpper++;
                 Console.Write("\n");     
             }
 
